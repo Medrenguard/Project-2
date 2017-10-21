f5d6a412ef366704b626a495874e92f547352ed1
Durkgame <medrenguard@gmail.com>
2017-10-21 22:11:55 +0400

    Все круто зарефакторил, распаралеллил и добавил защиту от дурака(need UPD)

--- main.js
+++ main.js
@@ -3,41 +3,60 @@ var u = new Url();
 var a = document.querySelectorAll('a'); //pre
 var lm = leftMenu;
 var el1 = content;
+var el2;
+var el3;
 var di;
 var tmp1;
-var count = 0;
-function map(trig, h, notion, id) {
-	if (trig === 0) {
-		lm.innerHTML = '<ul>';
-		for (var i2 = 0; i2 < h.length && h[i2][1]; i2++) {
-			document.querySelector('#leftMenu ul').innerHTML += '<li><a tax="t=' + h[i2][0] + '" pr="2" data-index=' + i2 + '>' + h[i2][1] + '</a></li>';
-			count++;
-		};
-		a = document.querySelectorAll('a');	
-	} else if (trig === 1) {
-		switch(h) {
-			case 'text':
-			tmp1 = text;
-			break;
-			case 'img':
-			tmp1 = img;
-			break;
-			case 'audio':
-			tmp1 = audio;
-			break;
-			case 'video':
-			tmp1 = video;
-			break;
-			case 'anim':
-			tmp1 = anim;
-			break;
-			case 'interactive':
-			tmp1 = interactive;
-			break;
-		};
-		di = document.querySelector('[tax="t='+notion+'"]').getAttribute('data-index');
-		el1.innerHTML = '<b>' + tmp1[di][2] + '</b> <i>' + tmp1[di][3] + '</i> - ' + tmp1[di][4];
+
+function alertBug() {
+	el1.innerHTML = 'Упс! Такой страницы не существует.';
+};
+
+function findThisArr(value) {
+	switch(value) {
+		case 'text':
+		tmp1 = text;
+		break;
+		case 'img':
+		tmp1 = img;
+		break;
+		case 'audio':
+		tmp1 = audio;
+		break;
+		case 'video':
+		tmp1 = video;
+		break;
+		case 'anim':
+		tmp1 = anim;
+		break;
+		case 'interactive':
+		tmp1 = interactive;
+		break;
 	};
+	return tmp1;
+};
+
+function map(trig, h, notion, id) {
+	el2 = findThisArr(h);
+	el3 = document.querySelector('[tax="t='+notion+'"]');
+	if (trig === 0) {
+		if (el2) {
+			lm.innerHTML = '<ul>';
+			for (var i2 = 0; i2 < el2.length && el2[i2][1]; i2++) {
+				document.querySelector('#leftMenu ul').innerHTML += '<li><a tax="t=' + el2[i2][0] + '" pr="2" data-index=' + i2 + '>' + el2[i2][1] + '</a></li>';
+			};
+		} else {
+			alertBug();
+		};
+	} else if (trig === 1) {
+		if (el3) {
+			di = el3.getAttribute('data-index');
+			el1.innerHTML = '<b>' + el2[di][2] + '</b> <i>' + el2[di][3] + '</i> - ' + el2[di][4];
+		} else {
+			alertBug();
+		};
+	};
+	a = document.querySelectorAll('a');
 };
 
 updFields();
@@ -58,6 +77,7 @@ function updA() {
 		};
 	};
 };
+
 function updUrl(e) {
 	if (e.target.getAttribute('pr') == 1) {
 		history.pushState('','',u.path+'?'+e.target.getAttribute('tax'));
@@ -65,32 +85,21 @@ function updUrl(e) {
 		history.pushState('','',u.path+'?c='+u.query.c+'&'+e.target.getAttribute('tax'));
 	};
 	u = new Url();
-	count++;
 };
+
 function updFields(e) {
-	switch(u.query.c){
-		case 'text':
-		map(0, text);
-		break;
-		case 'img':
-		map(0, img);
-		break;
-		case 'audio':
-		map(0, audio);
-		break;
-		case 'video':
-		map(0, video);
-		break;
-		case 'anim':
-		map(0, anim);
-		break;
-		case 'interactive':
-		map(0, interactive);
-		break;
-	};
-	if (e) {
-		console.log(e.target);
-	} else if (u.query.t) {
-		map(1, u.query.c, u.query.t);
-	};
+	if (!e) {
+		if (u.query.c) {
+			map(0, u.query.c);
+		};
+		if (u.query.t) {
+			map(1, u.query.c, u.query.t);
+		};
+	} else {
+		if (e.target.getAttribute('pr') == 1) {
+			map(0, u.query.c);
+		} else if (e.target.getAttribute('pr') == 2) {
+			map(1, u.query.c, u.query.t);
+		};
+	}
 };
