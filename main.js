62cb176415a4dd79896166b855926232a3160a07
Durkgame <medrenguard@gmail.com>
2017-10-21 15:02:47 +0400

    Фикс клонирующихся EventListener-ов(endless for). Upd to onckick?

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-"use strict;"
+"use strict";
 var u = new Url();
 var a = document.querySelectorAll('a'); //pre
 var lm = leftMenu;
 var el1 = content;
-
+var count = 0;
 function map(h, notion, id) {
 	lm.innerHTML = '<ul>';
-	for (var i2 = 0; i2 < h.length && h[i2][1]; i2++) {
+	for (var i2 = 0; h[i2][1]; i2++) {
 		document.querySelector('#leftMenu ul').innerHTML += '<li><a tax="t=' + h[i2][0] + '" pr="2">' + h[i2][1] + '</a></li>';
+		count++;
 	};
 	a = document.querySelectorAll('a');
 };
@@ -36,39 +37,43 @@ switch(u.query.c){
 document.body.addEventListener('click', updA);
 
 function updA() {
+	for (var i = 0; i < a.length; i++) {
+		a[i].removeEventListener('click', feed)
+	};
 	a = document.querySelectorAll('a');
 	for (var i = 0; i < a.length; i++) {
-		a[i].addEventListener('click', function(e) {
-			if (event.target.getAttribute('pr') == 1) {
-				history.pushState('','',u.path+'?'+this.getAttribute('tax'));
-			} else if (event.target.getAttribute('pr') == 2) {
-				history.pushState('','',u.path+'?c='+u.query.c+'&'+this.getAttribute('tax'));
-			};
-			u = new Url();
+		a[i].addEventListener('click', feed)
+	};
+};
+function feed(e) {
+	if (event.target.getAttribute('pr') == 1) {
+		history.pushState('','',u.path+'?'+this.getAttribute('tax'));
+	} else if (event.target.getAttribute('pr') == 2) {
+		history.pushState('','',u.path+'?c='+u.query.c+'&'+this.getAttribute('tax'));
+	};
+	u = new Url();
+	count++;
+	//
 
-			//
-
-			switch(u.query.c){
-				case 'text':
-				map(text);
-				break;
-				case 'img':
-				map(img);
-				break;
-				case 'audio':
-				map(audio);
-				break;
-				case 'video':
-				map(video);
-				break;
-				case 'anim':
-				map(anim);
-				break;
-				case 'interactive':
-				map(interactive);
-				break;
-			};
-		});
-	}
-}
+	switch(u.query.c){
+		case 'text':
+		map(text);
+		break;
+		case 'img':
+		map(img);
+		break;
+		case 'audio':
+		map(audio);
+		break;
+		case 'video':
+		map(video);
+		break;
+		case 'anim':
+		map(anim);
+		break;
+		case 'interactive':
+		map(interactive);
+		break;
+	};
+};
 updA();
