e8fa542a760b985de7d92ef8151644962c89bb0b
Durkgame <medrenguard@gmail.com>
2017-10-21 16:18:40 +0400

    addEventListener(click) UPD to onclick. И рефакторинг О_о

--- main.js
+++ main.js
@@ -6,55 +6,41 @@ var el1 = content;
 var count = 0;
 function map(h, notion, id) {
 	lm.innerHTML = '<ul>';
-	for (var i2 = 0; h[i2][1]; i2++) {
+	for (var i2 = 0; i2 < h.length && h[i2][1]; i2++) {
 		document.querySelector('#leftMenu ul').innerHTML += '<li><a tax="t=' + h[i2][0] + '" pr="2">' + h[i2][1] + '</a></li>';
 		count++;
 	};
 	a = document.querySelectorAll('a');
 };
 
-switch(u.query.c){
-	case 'text':
-	map(text);
-	break;
-	case 'img':
-	map(img);
-	break;
-	case 'audio':
-	map(audio);
-	break;
-	case 'video':
-	map(video);
-	break;
-	case 'anim':
-	map(anim);
-	break;
-	case 'interactive':
-	map(interactive);
-	break;
-};
+updFields();
+
+updA();
 
 document.body.addEventListener('click', updA);
 
 function updA() {
-	for (var i = 0; i < a.length; i++) {
-		a[i].removeEventListener('click', feed)
-	};
 	a = document.querySelectorAll('a');
 	for (var i = 0; i < a.length; i++) {
-		a[i].addEventListener('click', feed)
+		a[i].onclick = function (e) {
+
+			updUrl(e);
+
+			updFields(e);
+
+		};
 	};
 };
-function feed(e) {
-	if (event.target.getAttribute('pr') == 1) {
-		history.pushState('','',u.path+'?'+this.getAttribute('tax'));
-	} else if (event.target.getAttribute('pr') == 2) {
-		history.pushState('','',u.path+'?c='+u.query.c+'&'+this.getAttribute('tax'));
+function updUrl(e) {
+	if (e.target.getAttribute('pr') == 1) {
+		history.pushState('','',u.path+'?'+e.target.getAttribute('tax'));
+	} else if (e.target.getAttribute('pr') == 2) {
+		history.pushState('','',u.path+'?c='+u.query.c+'&'+e.target.getAttribute('tax'));
 	};
 	u = new Url();
 	count++;
-	//
-
+};
+function updFields(e) {
 	switch(u.query.c){
 		case 'text':
 		map(text);
@@ -76,4 +62,3 @@ function feed(e) {
 		break;
 	};
 };
-updA();
